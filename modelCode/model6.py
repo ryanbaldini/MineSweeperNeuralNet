d071031ebef17cde3baec4debf1a1ed120dcf3fa
Ryan Baldini <ryan.baldini@seagate.com>
2017-08-20 11:39:45 -0600

    now playing on “Expert” game board

--- modelCode/model6.py
+++ modelCode/model6.py
@@ -3,11 +3,12 @@ from keras.layers import Input
 from keras.layers.convolutional import Conv2D
 from keras.layers.merge import Multiply
 
-dim = 10
-inputShape = (11,dim,dim)   #11 channels, each 10x10
+dim1 = 16
+dim2 = 30
+inputShape = (11,dim1,dim2)   #11 channels
 
 in1 = Input(shape=inputShape)
-in2 = Input(shape=(1,dim,dim))
+in2 = Input(shape=(1,dim1,dim2))
 conv = Conv2D(64, (3,3), padding='same', data_format = 'channels_first', activation = 'relu', use_bias = True)(in1)
 conv = Conv2D(64, (3,3), padding='same', data_format = 'channels_first', activation = 'relu', use_bias = True)(conv)
 conv = Conv2D(64, (3,3), padding='same', data_format = 'channels_first', activation = 'relu', use_bias = True)(conv)
